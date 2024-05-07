5cb69f94e1cc8cf6aac0d22c74a2dc23ea641e24
Rodri <156032416+Rodri-Perez@users.noreply.github.com>
2024-05-07 22:31:56 +0000

    Update dinamic.js

--- dinamic.js
+++ dinamic.js
@@ -1,14 +1,1 @@
-const bootstrap = document.querySelector('.boot');
-const style = document.querySelector('.style');
 
-bootstrap.addEventListener("onclick", ()=>{
-
-  location.href("https://rodri-perez.github.io/plataformas-moviles-entregas/indice.html");
-               
-});
-
-style.addEventListener("onclick", ()=>{
-
-  location.href("https://rodri-perez.github.io/plataformas-moviles-entregas/index.html");
-        
-});
